1fc4e475a7f5041e17c0f939a4a5739571d14d04
neywen <neywen@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2008-08-25 22:12:24 +0000

    fixed some file headers in nui3
    git-svn-id: svn://libnui.net/nui/trunk/nui3@3694 9db30882-7939-4ce0-a36a-45f8a15c01b5

--- src/Window/Win/nuiMainMenu_WinXX.cpp
+++ src/Window/Win/nuiMainMenu_WinXX.cpp
@@ -1,40 +1,11 @@
 /*
- NUI - C++ cross-platform GUI framework for OpenGL based applications
- Copyright (C) 2002-2003 SÈbastien MÈtrot
- 
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
- 
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
- 
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  NUI3 - C++ cross-platform GUI framework for OpenGL based applications
+  Copyright (C) 2002-2003 Sebastien Metrot
+
+  licence: see nui3/LICENCE.TXT
+  Copyright (C) 2000-2003 NGL Team
  */
 
-/*
- NUI - C++ cross-platform GUI framework for OpenGL based applications
- Copyright (C) 2002-2003 SÃˆbastien MÃˆtrot
- 
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
- 
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
- 
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
 
 #include "nui.h"
 #include "nuiMainMenu.h"
