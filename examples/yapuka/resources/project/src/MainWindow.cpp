4bb4dc347959ade85f96d121dc83d9461c036f3b
neywen <neywen@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2008-09-04 09:50:41 +0000

    fixed yapuka project generator
    git-svn-id: svn://libnui.net/nui/trunk/nui3@3732 9db30882-7939-4ce0-a36a-45f8a15c01b5

--- examples/yapuka/resources/project/src/MainWindow.cpp
+++ examples/yapuka/resources/project/src/MainWindow.cpp
@@ -16,7 +16,7 @@
  */
 
 MainWindow::MainWindow(const nglContextInfo& rContextInfo, const nglWindowInfo& rInfo, bool ShowFPS, const nglContext* pShared )
-  : nuiMainWindow(rContextInfo, rInfo, pShared, nglPath(_T("../data"))), mEventSink(this)
+  : nuiMainWindow(rContextInfo, rInfo, pShared, nglPath(ePathCurrent)), mEventSink(this)
 {
 
 }
