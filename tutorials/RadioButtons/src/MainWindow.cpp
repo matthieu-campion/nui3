a3be14f5e2157189b1c2c596a7227e5dec905f95
neywen <neywen@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2008-09-26 13:38:51 +0000

    added 'RadioButtons' and 'ComboBox' tutorials projects : empty for now
    git-svn-id: svn://libnui.net/nui/trunk/nui3@3830 9db30882-7939-4ce0-a36a-45f8a15c01b5

---
+++ tutorials/RadioButtons/src/MainWindow.cpp
@@ -0,0 +1,43 @@
+/*
+  NUI3 - C++ cross-platform GUI framework for OpenGL based applications
+  Copyright (C) 2002-2003 Sebastien Metrot
+
+  licence: see nui3/LICENCE.TXT
+*/
+
+#include "nui.h"
+#include "MainWindow.h"
+#include "Application.h"
+#include "nuiLabel.h"
+
+
+/*
+ * MainWindow
+ */
+
+MainWindow::MainWindow(const nglContextInfo& rContextInfo, const nglWindowInfo& rInfo, bool ShowFPS, const nglContext* pShared )
+  : nuiMainWindow(rContextInfo, rInfo, pShared, nglPath(ePathCurrent)), mEventSink(this)
+{
+
+}
+
+MainWindow::~MainWindow()
+{
+}
+
+void MainWindow::OnCreation()
+{
+  AddChild(new nuiLabel(_T("hello world!")));
+}
+
+void MainWindow::OnClose()
+{
+  if (GetNGLWindow()->IsInModalState())
+    return;
+  
+  
+  App->Quit();
+}
+
+
+
