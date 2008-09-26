e593f0d7d38e81942f27f5733737942d17345bbb
meeloo <meeloo@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2008-09-26 14:41:09 +0000

    adding test rasterizer application
    git-svn-id: svn://libnui.net/nui/trunk/nui3@3831 9db30882-7939-4ce0-a36a-45f8a15c01b5

---
+++ examples/testrasterizer/src/MainWindow.cpp
@@ -0,0 +1,45 @@
+/*
+  NUI3 - C++ cross-platform GUI framework for OpenGL based applications
+  Copyright (C) 2002-2003 Sebastien Metrot
+
+  licence: see nui3/LICENCE.TXT
+*/
+
+#include "nui.h"
+#include "MainWindow.h"
+#include "Application.h"
+#include "nuiLabel.h"
+
+
+/*
+ * MainWindow
+ */
+
+MainWindow::MainWindow(const nglContextInfo& rContextInfo, const nglWindowInfo& rInfo, bool ShowFPS, const nglContext* pShared )
+  : nuiMainWindow(rContextInfo, rInfo, pShared, nglPath(ePathCurrent)), mEventSink(this)
+{
+
+}
+
+MainWindow::~MainWindow()
+{
+}
+
+void MainWindow::OnCreation()
+{
+  nuiImage* pImage = new nuiImage(_T("rsrc:/rasters2.png"));
+  pImage->SetPosition(nuiCenter);
+  AddChild(pImage);
+}
+
+void MainWindow::OnClose()
+{
+  if (GetNGLWindow()->IsInModalState())
+    return;
+  
+  
+  App->Quit();
+}
+
+
+
