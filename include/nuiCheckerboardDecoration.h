2fdac5c3f1db934a094db7297957b8b5705caea8
neywen <neywen@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2008-09-29 14:35:41 +0000

    added nuiCheckerboardDecoration, added Checkerboard tutorial
    git-svn-id: svn://libnui.net/nui/trunk/nui3@3847 9db30882-7939-4ce0-a36a-45f8a15c01b5

---
+++ include/nuiCheckerboardDecoration.h
@@ -0,0 +1,61 @@
+/*
+  NUI3 - C++ cross-platform GUI framework for OpenGL based applications
+  Copyright (C) 2002-2003 Sebastien Metrot
+
+  licence: see nui3/LICENCE.TXT
+*/
+
+#pragma once
+
+#include "nui.h"
+#include "nuiDecoration.h"
+
+
+class nuiCheckerboardDecoration : public nuiDecoration
+{
+public:
+
+  nuiCheckerboardDecoration(const nglString& rName);
+  virtual ~nuiCheckerboardDecoration();
+  
+  bool Load(const nuiXMLNode* pNode);
+  nuiXMLNode* Serialize(nuiXMLNode* pNode);
+  
+
+  virtual nuiSize GetBorder(nuiPosition position) const;
+  virtual nuiRect GetIdealClientRect() const;
+
+  void SetSourceClientRect(const nuiRect& rRect);
+  const nuiRect& GetSourceClientRect() const;
+
+  const nuiColor& GetBackgroundColor() const;
+  void SetBackgroundColor(const nuiColor& color);
+  
+  const nuiColor& GetTileColor() const;
+  void SetTileColor(const nuiColor& color);
+  
+  void SetTilesPerLine(uint32 nb);
+  uint32 GetTilesPerLine() const;
+
+  const nuiColor& GetStrokeColor() const;
+	void SetStrokeColor(const nuiColor& color);
+
+  void SetStrokeSize(uint32 size);
+  uint32 GetStrokeSize() const;
+
+private:
+
+  virtual void Draw(nuiDrawContext* pContext, nuiWidget* pWidget, const nuiRect& rRect);
+
+  void InitAttributes();
+
+  uint32 mStrokeSize;
+  uint32 mTilesPerLine;
+  nuiColor mStrokeColor;
+  nuiColor mTileColor;
+  nuiColor mBackgroundColor;
+  nuiBlendFunc mBlendFunc;
+  nuiRect mClientRect;
+
+};
+
---
+++ src/Decorations/nuiCheckerboardDecoration.cpp
@@ -0,0 +1,252 @@
+/*
+  NUI3 - C++ cross-platform GUI framework for OpenGL based applications
+  Copyright (C) 2002-2003 Sebastien Metrot
+
+  licence: see nui3/LICENCE.TXT
+*/
+
+#include "nui.h"
+#include "nuiCheckerboardDecoration.h"
+
+nuiCheckerboardDecoration::nuiCheckerboardDecoration(const nglString& rName)
+: nuiDecoration(rName)
+{
+  if (SetObjectClass(_T("nuiCheckerboardDecoration")))
+    InitAttributes();
+
+  mStrokeSize = 1;
+  mStrokeColor = nuiColor(0,0,0);
+  mClientRect = nuiRect(0,0,0,0);
+}
+
+nuiCheckerboardDecoration::~nuiCheckerboardDecoration()
+{
+}
+
+
+void nuiCheckerboardDecoration::InitAttributes()
+{
+  AddAttribute(new nuiAttribute<const nuiRect&>
+   (nglString(_T("ClientRect")), nuiUnitNone,
+    nuiAttribute<const nuiRect&>::GetterDelegate(this, &nuiCheckerboardDecoration::GetSourceClientRect),
+    nuiAttribute<const nuiRect&>::SetterDelegate(this, &nuiCheckerboardDecoration::SetSourceClientRect)));
+
+  AddAttribute(new nuiAttribute<uint32>
+  (nglString(_T("StrokeSize")), nuiUnitPixels,
+   nuiFastDelegate::MakeDelegate(this, &nuiCheckerboardDecoration::GetStrokeSize),
+   nuiFastDelegate::MakeDelegate(this, &nuiCheckerboardDecoration::SetStrokeSize)));
+  
+  AddAttribute(new nuiAttribute<const nuiColor&>
+  (nglString(_T("StrokeColor")), nuiUnitColor,
+   nuiFastDelegate::MakeDelegate(this, &nuiCheckerboardDecoration::GetStrokeColor), 
+   nuiFastDelegate::MakeDelegate(this, &nuiCheckerboardDecoration::SetStrokeColor)));
+
+  AddAttribute(new nuiAttribute<const nuiColor&>
+               (nglString(_T("BackgroundColor")), nuiUnitColor,
+                nuiFastDelegate::MakeDelegate(this, &nuiCheckerboardDecoration::GetBackgroundColor), 
+                nuiFastDelegate::MakeDelegate(this, &nuiCheckerboardDecoration::SetBackgroundColor)));
+
+  AddAttribute(new nuiAttribute<uint32>
+               (nglString(_T("TilesPerLine")), nuiUnitColor,
+                nuiFastDelegate::MakeDelegate(this, &nuiCheckerboardDecoration::GetTilesPerLine), 
+                nuiFastDelegate::MakeDelegate(this, &nuiCheckerboardDecoration::SetTilesPerLine)));
+  
+  AddAttribute(new nuiAttribute<const nuiColor&>
+               (nglString(_T("TileColor")), nuiUnitColor,
+                nuiFastDelegate::MakeDelegate(this, &nuiCheckerboardDecoration::GetTileColor), 
+                nuiFastDelegate::MakeDelegate(this, &nuiCheckerboardDecoration::SetTileColor)));
+}
+
+
+
+bool nuiCheckerboardDecoration::Load(const nuiXMLNode* pNode)
+{
+  //#FIXME
+  return true;
+}
+
+nuiXMLNode* nuiCheckerboardDecoration::Serialize(nuiXMLNode* pNode)
+{
+//#FIXME
+  return pNode;
+}
+
+// virtual
+void nuiCheckerboardDecoration::Draw(nuiDrawContext* pContext, nuiWidget* pWidget, const nuiRect& rDestRect)
+{
+  pContext->EnableAntialiasing(false);
+  pContext->EnableBlending(true);
+  pContext->SetBlendFunc(nuiBlendTransp);
+  pContext->EnableTexturing(false);
+  
+  // background
+  pContext->SetFillColor(mBackgroundColor);
+  pContext->DrawRect(rDestRect, eFillShape);
+  
+  
+  // checkerboard
+  nuiSize clientWidth = rDestRect.GetWidth() - mStrokeSize;
+  nuiSize tileWidth = clientWidth / (float)mTilesPerLine;
+
+  nuiRect tileRect = nuiRect(mStrokeSize, mStrokeSize, tileWidth, tileWidth);
+  
+  pContext->SetFillColor(mTileColor);
+  
+  
+  // mTilesPerLine <=> nb of columns AND nb of rows
+  for (uint32 row = 0; row < mTilesPerLine; row++)
+  {
+    
+    uint32 firstCol = (row & 1)? 0 : 1;
+    
+    for (uint32 col = firstCol; col < mTilesPerLine; col+=2)
+    {
+      tileRect.MoveTo((col * tileWidth)+mStrokeSize, tileRect.Top());
+      pContext->DrawRect(tileRect, eFillShape);
+    }
+
+    // go to next line
+    tileRect.Move(0, tileWidth);   
+  }
+
+  
+  // border
+  if (mStrokeSize != 0)
+  {
+    pContext->SetLineWidth(mStrokeSize);  
+    pContext->SetStrokeColor(mStrokeColor);
+    pContext->DrawRect(rDestRect, eStrokeShape);
+  }
+  
+  
+}
+
+
+
+nuiRect nuiCheckerboardDecoration::GetIdealClientRect() const
+{
+  return mClientRect.Size();
+}
+
+
+
+nuiSize nuiCheckerboardDecoration::GetBorder(nuiPosition position) const
+{
+  switch (position)
+  {
+  case nuiLeft:
+  case nuiRight:
+    return mClientRect.Left();
+    break;
+  case nuiTop:
+  case nuiBottom:
+    return mClientRect.Top();
+    break;
+  case nuiFillHorizontal:
+    return mClientRect.Left()*2;
+    break;
+  case nuiFillVertical:
+    return mClientRect.Top()*2;
+    break;
+  case nuiNoPosition: break;
+  case nuiTopLeft: break;
+  case nuiTopRight: break;
+  case nuiBottomLeft: break;
+  case nuiBottomRight: break;
+  case nuiCenter: break;
+  case nuiTile: break;
+  case nuiFill: break;
+  case nuiFillLeft: break;
+  case nuiFillRight: break;
+  case nuiFillTop: break;
+  case nuiFillBottom: break;
+  }
+  //we should'nt arrive here
+  return NULL; 
+}
+
+
+
+//*************************************************************************
+//
+// attributes setters/getters
+//
+
+
+const nuiRect& nuiCheckerboardDecoration::GetSourceClientRect() const
+{
+  return mClientRect;
+}
+
+void nuiCheckerboardDecoration::SetSourceClientRect(const nuiRect& rRect)
+{
+  mClientRect = rRect;
+}
+
+
+
+const nuiColor& nuiCheckerboardDecoration::GetStrokeColor() const
+{
+  return mStrokeColor;
+}
+
+
+void nuiCheckerboardDecoration::SetStrokeColor(const nuiColor& color)
+{
+  mStrokeColor = color;
+}
+
+
+
+const nuiColor& nuiCheckerboardDecoration::GetTileColor() const
+{
+  return mTileColor;
+}
+
+
+void nuiCheckerboardDecoration::SetTileColor(const nuiColor& color)
+{
+  mTileColor = color;
+}
+
+
+
+
+
+const nuiColor& nuiCheckerboardDecoration::GetBackgroundColor() const
+{
+  return mBackgroundColor;
+}
+
+
+void nuiCheckerboardDecoration::SetBackgroundColor(const nuiColor& color)
+{
+  mBackgroundColor = color;
+}
+
+
+
+
+
+void nuiCheckerboardDecoration::SetStrokeSize(uint32 size)
+{
+  mStrokeSize = size;
+}
+
+
+uint32 nuiCheckerboardDecoration::GetStrokeSize() const
+{
+  return mStrokeSize;
+}
+
+
+void nuiCheckerboardDecoration::SetTilesPerLine(uint32 nb)
+{
+  mTilesPerLine = nb;
+}
+
+
+uint32 nuiCheckerboardDecoration::GetTilesPerLine() const
+{
+  return mTilesPerLine;
+}
