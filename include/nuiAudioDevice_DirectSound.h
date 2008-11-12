4b01a0f509e9deb686ee26502b22ffb515285ed8
charles <charles@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2008-11-12 15:30:26 +0000

    add missing file
    git-svn-id: svn://libnui.net/nui/trunk/nui3@4013 9db30882-7939-4ce0-a36a-45f8a15c01b5

---
+++ include/nuiAudioDevice_DirectSound.h
@@ -0,0 +1,65 @@
+/*
+NUI3 - C++ cross-platform GUI framework for OpenGL based applications
+Copyright (C) 2002-2003 Sebastien Metrot
+
+licence: see nui3/LICENCE.TXT
+*/
+
+#pragma once
+
+#include "nuiAudioDevice.h"
+
+#include <dsound.h>
+
+class nuiAudioDeviceAPI_DirectSound : public nuiAudioDeviceAPI
+{
+public:
+  nuiAudioDeviceAPI_DirectSound();
+
+  virtual ~nuiAudioDeviceAPI_DirectSound();
+
+  virtual uint32 GetDeviceCount() const;
+
+  virtual nuiAudioDevice* GetDevice(uint32 index);
+  virtual nuiAudioDevice* GetDevice(const nglString& rDeviceName);
+  virtual nglString GetDeviceName(uint32 index) const;
+  virtual nuiAudioDevice* GetDefaultInputDevice();
+  virtual nuiAudioDevice* GetDefaultOutputDevice();
+
+protected:
+
+  class DeviceDesc
+  {
+  public:
+    DeviceDesc()
+    {
+      mInputGUID = DSDEVID_DefaultCapture;
+      mOutputGUID = DSDEVID_DefaultPlayback;
+    }
+
+    GUID mInputGUID;
+    GUID mOutputGUID;
+    nglString mInName;
+    nglString mInModule;
+    nglString mOutName;
+    nglString mOutModule;
+  };
+
+
+  mutable std::vector<DeviceDesc> mDeviceIDs;
+  mutable std::map<nglString, int32> mDeviceMap;
+
+
+
+private:
+  BOOL RealDSEnumInputCallback(LPGUID lpGuid, LPCWSTR lpcstrDescription, LPCWSTR lpcstrModule);
+
+  static BOOL CALLBACK DSEnumInputCallback(LPGUID lpGuid, LPCWSTR lpcstrDescription, LPCWSTR lpcstrModule, LPVOID lpContext);
+
+  BOOL RealDSEnumOutputCallback(LPGUID lpGuid, LPCWSTR lpcstrDescription, LPCWSTR lpcstrModule);
+
+  static BOOL CALLBACK DSEnumOutputCallback(LPGUID lpGuid, LPCWSTR lpcstrDescription, LPCWSTR lpcstrModule, LPVOID lpContext);
+  
+};
+
+extern nuiAudioDeviceAPI_DirectSound DirectSoundApi;
