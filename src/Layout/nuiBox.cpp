2013d99e1142a5021acd0b44ec2ee4e0ad132d85
meeloo <meeloo@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2008-12-20 01:40:27 +0000

    forgot to add this file...
    git-svn-id: svn://libnui.net/nui/trunk/nui3@4167 9db30882-7939-4ce0-a36a-45f8a15c01b5

---
+++ src/Layout/nuiBox.cpp
@@ -0,0 +1,60 @@
+/*
+ NUI3 - C++ cross-platform GUI framework for OpenGL based applications
+ Copyright (C) 2002-2003 Sebastien Metrot
+ 
+ licence: see nui3/LICENCE.TXT
+ */
+
+#include "nui.h"
+#include "nuiBox.h"
+
+void nuiBox::InitBoxAttributes()
+{
+  AddAttribute(new nuiAttribute<nuiSize>
+               (nglString(_T("CellMaxGrow")), nuiUnitSize,
+                nuiMakeDelegate(this, &nuiBox::GetCellMaxGrow),
+                nuiMakeDelegate(this, &nuiBox::SetCellMaxGrow),
+                nuiMakeDelegate(this, &nuiBox::GetBoxDimensionRange)));
+
+  AddAttribute(new nuiAttribute<nuiSize>
+               (nglString(_T("CellMaxPixels")), nuiUnitSize,
+                nuiMakeDelegate(this, &nuiBox::GetCellMaxPixels),
+                nuiMakeDelegate(this, &nuiBox::SetCellMaxPixels),
+                nuiMakeDelegate(this, &nuiBox::GetBoxDimensionRange)));
+  
+  AddAttribute(new nuiAttribute<nuiSize>
+               (nglString(_T("CellMinPixels")), nuiUnitSize,
+                nuiMakeDelegate(this, &nuiBox::GetCellMinPixels),
+                nuiMakeDelegate(this, &nuiBox::SetCellMinPixels),
+                nuiMakeDelegate(this, &nuiBox::GetBoxDimensionRange)));
+  
+  AddAttribute(new nuiAttribute<nuiSize>
+               (nglString(_T("CellMaxShrink")), nuiUnitSize,
+                nuiMakeDelegate(this, &nuiBox::GetCellMaxShrink),
+                nuiMakeDelegate(this, &nuiBox::SetCellMaxShrink),
+                nuiMakeDelegate(this, &nuiBox::GetBoxDimensionRange)));
+  
+  AddAttribute(new nuiAttribute<nuiSize>
+               (nglString(_T("CellPixels")), nuiUnitSize,
+                nuiMakeDelegate(this, &nuiBox::GetCellPixels),
+                nuiMakeDelegate(this, &nuiBox::SetCellPixels),
+                nuiMakeDelegate(this, &nuiBox::GetBoxDimensionRange)));
+}
+
+uint32 nuiBox::GetBoxDimensionRange(uint32 dimension) const
+{
+  if (dimension == 0)
+    return GetNbCells();
+  return 0;
+}
+
+nuiBox::~nuiBox()
+{
+}
+
+nuiBox::nuiBox(uint32 nbcellsH, uint32 nbcellsV)
+: nuiGrid(nbcellsH, nbcellsV)
+{
+  InitBoxAttributes();
+}
+
