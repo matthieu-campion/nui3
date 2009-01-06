a3be4e95e31c8de04897b84cafd13633dd1f8cfc
neywen <neywen@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2009-01-06 13:47:51 +0000

    added "TabView" empty tutorial
    git-svn-id: svn://libnui.net/nui/trunk/nui3@4202 9db30882-7939-4ce0-a36a-45f8a15c01b5

---
+++ tutorials/TabView/src/MainWindow.cpp
@@ -0,0 +1,116 @@
+/*
+  NUI3 - C++ cross-platform GUI framework for OpenGL based applications
+  Copyright (C) 2002-2003 Sebastien Metrot
+
+  licence: see nui3/LICENCE.TXT
+*/
+
+#include "nui.h"
+#include "MainWindow.h"
+#include "Application.h"
+#include "nuiCSS.h"
+#include "nuiVBox.h"
+
+
+/*
+ * MainWindow
+ */
+
+MainWindow::MainWindow(const nglContextInfo& rContextInfo, const nglWindowInfo& rInfo, bool ShowFPS, const nglContext* pShared )
+  : nuiMainWindow(rContextInfo, rInfo, pShared, nglPath(ePathCurrent)), mEventSink(this)
+{
+#ifdef _DEBUG_
+  SetDebugMode(true);
+#endif
+  
+  LoadCSS(_T("rsrc:/css/main.css"));
+}
+
+MainWindow::~MainWindow()
+{
+}
+
+void MainWindow::OnCreation()
+{
+  // a vertical box for page layout
+  nuiVBox* pLayoutBox = new nuiVBox(0);
+  pLayoutBox->SetExpand(nuiExpandShrinkAndGrow);
+  AddChild(pLayoutBox);
+  
+  // image in the first box's cell
+  nuiImage* pImg = new nuiImage();
+  pImg->SetObjectName(_T("MyImage"));
+  pImg->SetPosition(nuiCenter);
+  pLayoutBox->AddCell(pImg);
+  pLayoutBox->SetCellExpand(pLayoutBox->GetNbCells()-1, nuiExpandShrinkAndGrow);
+  
+  // button in the second cell : we use the default decoration for this button, but you could use the css to assign your own decoration
+  nuiButton* pButton = new nuiButton();
+  pButton->SetPosition(nuiCenter);
+  pLayoutBox->AddCell(pButton);
+  pLayoutBox->SetCellExpand(pLayoutBox->GetNbCells()-1, nuiExpandShrinkAndGrow);
+  
+  // click event on button
+  mEventSink.Connect(pButton->Activated, &MainWindow::OnButtonClick);
+  
+  // label with border in the button (put the label string in the button's constructor if you don't need borders)
+  nuiLabel* pButtonLabel = new nuiLabel(_T("click!"));
+  pButtonLabel->SetPosition(nuiCenter);
+  pButtonLabel->SetBorder(8,8);
+  pButton->AddChild(pButtonLabel);
+
+  // label with decoration in the third cell
+  mMyLabel = new nuiLabel(_T("my label"));
+  mMyLabel->SetObjectName(_T("MyLabel"));
+  mMyLabel->SetPosition(nuiCenter);
+  pLayoutBox->AddCell(mMyLabel);
+  pLayoutBox->SetCellExpand(pLayoutBox->GetNbCells()-1, nuiExpandShrinkAndGrow);
+}
+
+
+
+bool MainWindow::OnButtonClick(const nuiEvent& rEvent)
+{
+  nglString message;
+  double currentTime = nglTime();
+  message.Format(_T("click time: %.2f"), currentTime);
+  mMyLabel->SetText(message);
+  
+  return true; // means the event is caught and not broadcasted
+}
+
+
+void MainWindow::OnClose()
+{
+  if (GetNGLWindow()->IsInModalState())
+    return;
+  
+  
+  App->Quit();
+}
+
+
+bool MainWindow::LoadCSS(const nglPath& rPath)
+{
+  nglIStream* pF = rPath.OpenRead();
+  if (!pF)
+  {
+    NGL_OUT(_T("Unable to open CSS source file '%ls'\n"), rPath.GetChars());
+    return false;
+  }
+  
+  nuiCSS* pCSS = new nuiCSS();
+  bool res = pCSS->Load(*pF, rPath);
+  delete pF;
+  
+  if (res)
+  {
+    nuiMainWindow::SetCSS(pCSS);
+    return true;
+  }
+  
+  NGL_OUT(_T("%ls\n"), pCSS->GetErrorString().GetChars());
+  
+  delete pCSS;
+  return false;
+}
