ce2e170de74e7bd0e7144dd2b24d530597adba75
mat <mat@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2009-01-21 15:20:17 +0000

    added missing files
    git-svn-id: svn://libnui.net/nui/trunk/nui3@4253 9db30882-7939-4ce0-a36a-45f8a15c01b5

---
+++ src/AudioSamples/nuiAiffReader.cpp
@@ -0,0 +1,275 @@
+/*
+ *  nuinuiAiffReader.cpp
+ *  nui3
+ *
+ *  Created by mat on 1/19/09.
+ *  Copyright 2009 Mxp4. All rights reserved.
+ *
+ */
+
+#include "nuiAiffReader.h"
+#include "nuiAudioConvert.h"
+#include "nuiChunksDefinitions.h"
+
+#ifdef WIN32
+#define HiWord(x) ((short)((long)(x) >> 16))
+#define LoWord(x) ((short)(x))
+#endif
+
+
+double ExtendedToDouble(extended x)
+{
+  double q;
+  uint32 mant,mant2;
+  int16 sign;
+  
+  sign = x.exp[0] & 0x8000;
+  sign = sign ? -1 : 1;
+  
+  x.exp[0] &= 0x7FFF;
+  
+  mant = ((uint32)x.man[0]) << 16;
+  mant += (uint16)x.man[1];
+  mant2 = ((uint32)x.man[2]) << 16;
+  mant2 += (uint16)x.man[3];
+  
+  q = sign * (double)mant * pow(2.,(double)(x.exp[0] - 0x401E));
+  q += sign * (double)mant2 * pow(2.,(double)(x.exp[0] - 0x403E));
+  
+  return q;
+}
+
+extended DoubleToExtended(double q)
+{
+  extended x;
+  uint32 mant, mant2;
+  int16 sign;
+  uint16 exp;
+  
+  sign = q<0 ? 0x8000 : 0;
+  
+  if (sign)
+  {
+    q = -q;
+  }
+  
+  exp = (uint16) (log(q)/log(2.));
+  
+  mant = (uint32)(q/pow(2., (double)exp-31.));
+  
+  q -= (double )mant * pow(2.,(double)exp-31.);
+  mant2 = (uint32)(q/pow(2.,(double)exp-63));
+  
+  exp += 0x3FFF;
+  exp |= sign;
+  
+  x.exp[0] = exp;
+  x.man[0] = HiWord(mant);
+  x.man[1] = LoWord(mant);
+  x.man[2] = HiWord(mant2);
+  x.man[3] = LoWord(mant2);
+  
+  return x;
+}
+
+
+nuiAiffReader::nuiAiffReader(nglIStream& rStream) :
+nuiChunkSampleReader(rStream)
+{
+  mrStream.SetEndian(eEndianBig);
+}
+
+nuiAiffReader::nuiAiffReader(const nuiAiffReader& rReader, nglIStream& rStream) :
+nuiChunkSampleReader(rReader, rStream)
+{
+  mrStream.SetEndian(eEndianBig);
+}
+
+nuiAiffReader::~nuiAiffReader()
+{
+}
+
+nuiSampleReader* nuiAiffReader::Clone(nglIStream& rStream) const
+{
+  nuiAiffReader* pReader = new nuiAiffReader(*this, rStream);
+  pReader->SetPosition(0);
+  return pReader;
+}
+
+void nuiAiffReader::SetPosition(uint32 position)
+{
+  if (!mInitialized)
+    return;
+  
+  Chunk* pDataChunk = GetChunk("SSND");
+  NGL_ASSERT(pDataChunk);
+  nglFileOffset StreamPosition = pDataChunk->mDataPosition + position;
+  mrStream.SetPos(StreamPosition);
+  mPosition = position;
+}
+
+bool nuiAiffReader::ReadInfo()
+{
+  ScanAllChunks();
+  
+  //there are 8 unused bytes at the beginning of the "SSND" chunk in an AIFF file
+  Chunk* pSSNDChunk = GetChunk("SSND");
+  if (!pSSNDChunk)
+    return false;
+  pSSNDChunk->mDataPosition += 8;
+  
+  bool res = false;
+  
+  res = GoToChunk("FORM");
+  if (!res)
+    return false;
+  std::vector<char> fileFormat(4);
+  char* waveFormat = "AIFF";
+  for(uint32 i = 0; i < 4; i++)
+  {
+    if( 1 != mrStream.ReadUInt8((uint8*)&(fileFormat[i]), 1))
+      return false;
+    if (fileFormat[i] != waveFormat[i])
+      return false; // this stream is not a wave stream
+  }
+  mInfo.SetFileFormat(eAudioWave);
+  
+  res = GoToChunk("COMM");
+  if (!res)
+    return false;
+  
+  int16 Channels = 0;
+  uint32 SampleFrames = 0;
+  int16 SampleSize = 0;
+  extended SampleRate;
+  
+  //Read all parameters given in "COMM" chunk
+  if( 1 != mrStream.ReadInt16(&Channels,1))
+    return false;
+  if( 1 != mrStream.ReadUInt32(&SampleFrames,1))
+    return false;
+  if( 1 != mrStream.ReadInt16(&SampleSize,1))
+    return false;
+  
+  //read SampleRate exponent
+  if( 1 != mrStream.ReadInt16(SampleRate.exp,1))
+    return false;
+  //read Samplerate Mantisse
+  if( 4 != mrStream.ReadInt16(SampleRate.man,4))
+    return false;
+  
+  
+  mInfo.SetFormatTag(AIFF_FORMAT_TAG);
+  
+  mInfo.SetSampleRate(ExtendedToDouble(SampleRate));
+  mInfo.SetChannels(Channels);
+  mInfo.SetBitsPerSample(SampleSize);
+  mInfo.SetSampleFrames(SampleFrames);
+  
+  mInfo.SetStartFrame(0);
+  mInfo.SetStopFrame(mInfo.GetSampleFrames());
+  
+  return true;
+}
+
+uint32 nuiAiffReader::ReadIN(void* pBuffer, uint32 SampleFrames, nuiSampleBitFormat format)
+{
+  const uint32 channels = mInfo.GetChannels();
+  const uint64 SamplePointsToRead = SampleFrames * channels;
+  uint32 SampleFramesRead = 0;
+  
+  switch(format)
+  {
+    case eSampleInt16 :
+    {
+      switch (mInfo.GetBitsPerSample())
+      {
+        case 8 :
+        {
+          int16* pTempInt16 = (int16*)pBuffer;
+          uint32 sizeRead = mrStream.ReadUInt8( ((uint8*)pTempInt16) + SamplePointsToRead , SamplePointsToRead);
+          SampleFramesRead = sizeRead / channels;
+          nuiAudioConvert_Signed8bitsBufferTo16Bits(pTempInt16, sizeRead);
+        }
+          break;
+          
+        case 16 :
+        {
+          uint32 sizeRead = mrStream.ReadInt16( (int16*)pBuffer, SamplePointsToRead);
+          SampleFramesRead = sizeRead / channels;
+        }
+          break;
+          
+        case 24 :
+        case 32 :
+        default:
+          SampleFramesRead = 0;
+          break;
+      }
+    }
+      break;
+      
+    case eSampleFloat32 :
+    {
+      switch ( mInfo.GetBitsPerSample() )
+      {
+        case 8 :
+        {
+          float* pTempFloat = (float*)pBuffer;
+          uint32 sizeRead = mrStream.ReadUInt8( (uint8*)pTempFloat + 3 * SamplePointsToRead , SamplePointsToRead);
+          SampleFramesRead = sizeRead / channels;
+          nuiAudioConvert_Signed8bitsBufferToFloat(pTempFloat, sizeRead);
+        }
+          break;
+          
+        case 16 :
+        {
+          float* pTempFloat = (float*)pBuffer;
+          uint32 sizeRead = mrStream.ReadInt16( (int16*)pTempFloat + SamplePointsToRead, SamplePointsToRead);
+          SampleFramesRead = sizeRead / channels;
+          nuiAudioConvert_16bitsBufferToFloat(pTempFloat, SamplePointsToRead);
+        }
+          break;
+          
+        case 24 :
+        {
+          float* pTempFloat = (float*)pBuffer;
+          uint8* pTempBuffer;
+          pTempBuffer = new uint8[SamplePointsToRead * 3]; //nb of sample points * 3 bytes (24 bits) = nb of bytes to read
+          
+          uint32 sizeRead = mrStream.ReadUInt8(pTempBuffer, SamplePointsToRead * 3);
+          SampleFramesRead = sizeRead / channels / 3;
+          for (uint32 i = 0; i < sizeRead / 3; i++)
+          {
+            pTempFloat[i] = nuiAudioConvert_24bitsToFloatFromBigEndian(pTempBuffer + (3 * i));
+          }
+          
+          delete[] pTempBuffer;
+        }
+          break;
+          
+        case 32 :
+        {
+          uint32 sizeRead = mrStream.ReadFloat((float*)pBuffer, SamplePointsToRead);
+          SampleFramesRead = sizeRead / channels;
+          mPosition += SampleFramesRead;
+        }
+          break;
+          
+        default:
+          SampleFramesRead = 0;
+          break;
+      }
+    }
+      break;
+      
+    default :
+      SampleFramesRead = 0;
+      break;
+  }
+  
+  mPosition += SampleFramesRead;
+  return SampleFramesRead;
+}
+
+
---
+++ src/AudioSamples/nuiChunkSampleReader.cpp
@@ -0,0 +1,268 @@
+/*
+ *  nuiChunkSampleReader.cpp
+ *  nui3
+ *
+ *  Created by mat on 1/19/09.
+ *  Copyright 2009 Mxp4. All rights reserved.
+ *
+ */
+
+#include "nuiChunkSampleReader.h"
+
+#define CHUNK_ID_BYTES 4
+#define CHUNK_SIZE_BYTES 4
+#define CHUNK_HEADER_BYTES (CHUNK_ID_BYTES + CHUNK_SIZE_BYTES)
+#define FIRST_CHUNK_DATA_BYTES 4 //4 bytes to represent RIFF type (WAVE, AIFF...)
+//
+//
+//nuiChunkSampleReader
+//
+//
+
+nuiChunkSampleReader::nuiChunkSampleReader(nglIStream& rStream) : 
+nuiSampleReader(rStream)
+{
+}
+
+nuiChunkSampleReader::nuiChunkSampleReader(const nuiChunkSampleReader& rReader, nglIStream& rStream) :
+nuiSampleReader(rReader, rStream)
+{
+  for (uint32 i = 0; i < rReader.mChunks.size(); i++)
+  {
+    Chunk* pChunk = new Chunk(rReader.mChunks[i]->mPosition, rReader.mChunks[i]->mDataSize, rReader.mChunks[i]->mDataPosition, rReader.mChunks[i]->mId);
+    mChunks.push_back(pChunk);
+  }
+}
+
+nuiChunkSampleReader::~nuiChunkSampleReader()
+{
+  ClearChunks();
+}
+
+
+bool nuiChunkSampleReader::GoToChunk(char* pId)
+{
+  bool found = false;
+  uint32 index = 0;
+  for (uint32 i = 0; i < mChunks.size() && !found; i++)
+  {
+    found = mChunks[i]->CompareId(pId);
+    if (found)
+      index = i;
+  }
+  
+  if (found)
+  {
+    //Set Stream position
+    nglFileOffset pos = mChunks[index]->mDataPosition;
+    mrStream.SetPos(pos);
+  }
+  
+  return found;
+}
+
+nuiChunkSampleReader::Chunk* nuiChunkSampleReader::GetChunk(char* Id)
+{
+  bool found = false;
+  uint32 index = 0;
+  for (uint32 i = 0; i < mChunks.size() && !found; i++)
+  {
+    found = mChunks[i]->CompareId(Id);
+    if (found)
+      index = i;
+  }
+  
+  if (found)
+  {
+    Chunk* pChunk = mChunks[index];
+    return pChunk;
+  }
+  
+  return NULL;
+}
+
+bool nuiChunkSampleReader::ScanAllChunks()
+{  
+  ClearChunks();
+  
+  /////////////
+  //FIRST CHUNK
+  mrStream.SetPos(0);
+  
+  //get Id
+  //std::vector<char> Id(4);
+  char* Id = new char[5];
+  for(uint32 k = 0; k < 4; k++)
+  {
+    if( 1 != mrStream.ReadUInt8((uint8*)&(Id[k]), 1))
+      return false;
+  }
+  Id[4] = '\0';
+  //get first chunk size
+  uint32 utilSize = 0; // = (total size of the stream) - (first chunk header size)
+  if (1 != mrStream.ReadUInt32(&utilSize, 1))
+    return false;
+  uint32 firstChunkSize = FIRST_CHUNK_DATA_BYTES;
+
+  Chunk* pFirstChunk = new Chunk(0, firstChunkSize, CHUNK_HEADER_BYTES, Id);
+  mChunks.push_back(pFirstChunk);
+  nglFileOffset TotalSize = CHUNK_HEADER_BYTES  + firstChunkSize + utilSize;
+  
+  do
+  {
+    ////////////
+    //NEXT CHUNK
+    nglFileOffset ChunkPosition = mChunks.back()->mDataPosition + mChunks.back()->mDataSize;
+    mrStream.SetPos(ChunkPosition);
+    uint32 ChunkSize = 0;
+    for(uint32 k = 0; k < 4; k++)
+    {
+      if( 1 != mrStream.ReadUInt8((uint8*)&(Id[k]), 1))
+        return false;
+    }
+    //get chunk size
+    if (1 != mrStream.ReadUInt32(&ChunkSize, 1))
+      return false;
+    if (0 != ChunkSize % 2)
+          ChunkSize += 1; // the chunk size is always even
+    Chunk* pNextChunk = new Chunk(ChunkPosition, ChunkSize, ChunkPosition + CHUNK_HEADER_BYTES, Id);
+    mChunks.push_back(pNextChunk);
+  }
+  while (mChunks.back()->mDataPosition + mChunks.back()->mDataSize < TotalSize);
+
+  delete[] Id;
+  return true;
+}
+
+void nuiChunkSampleReader::ClearChunks()
+{
+  for (uint32 i = 0; i < mChunks.size(); i++)
+  {
+    delete mChunks[i];
+    mChunks[i] = NULL;
+  }
+  mChunks.clear();
+}
+
+
+uint32 nuiChunkSampleReader::ReadDE(std::vector<void*> buffers, uint32 sampleframes, nuiSampleBitFormat format)
+{
+  //don't increment mPosition: it's already done in ReadIN
+  const uint32 channels = mInfo.GetChannels();
+  
+  if (buffers.size() != channels)
+  {
+    return 0;
+  }
+  
+  bool deleteBuffer = false;
+  uint32 SampleFramesRead = 0;
+  switch (format)
+  {
+    case eSampleFloat32:
+    {
+      float* pFloatBuffer = NULL;
+      if (channels == 1)
+      {
+        pFloatBuffer = (float*)(buffers[0]);
+        deleteBuffer = false;
+      }
+      else
+      {
+        pFloatBuffer = new float[sampleframes * channels];
+        deleteBuffer = true;
+      }
+      
+      SampleFramesRead = ReadIN((void*)pFloatBuffer, sampleframes, format); //mPosition is incremented inside
+      for (uint32 s = 0; s < SampleFramesRead; s++)
+      {
+        for (uint32 c = 0; c < channels; c++)
+        {
+          ((float*)(buffers[c]))[s] = pFloatBuffer[channels * s + c];
+        }
+      }
+      if (deleteBuffer)
+        delete[] pFloatBuffer;
+    }
+      break;
+      
+    case eSampleInt16:
+    {
+      int16* pIntBuffer = NULL;
+      if (channels == 1)
+      {
+        pIntBuffer = (int16*)(buffers[0]);
+        deleteBuffer = false;
+      }
+      else
+      {
+        pIntBuffer = new int16[sampleframes * channels];
+        deleteBuffer = true;
+      }
+      
+      SampleFramesRead = ReadIN((void*)pIntBuffer, sampleframes, format);//mPosition is incremented inside
+      for (uint32 s = 0; s < SampleFramesRead; s++)
+      {
+        for (uint32 c = 0; c < channels; c++)
+        {
+            ((int16*)(buffers[c]))[s] = pIntBuffer[channels * s + c];
+        }
+      }
+      if (deleteBuffer)
+        delete[] pIntBuffer;
+    }
+      break;
+      
+    default:
+      break;
+  }
+  
+  
+  //don't increment mPosition: it's already done in ReadIN
+  return SampleFramesRead;
+}
+
+
+//
+//
+//Chunk
+//
+//
+nuiChunkSampleReader::Chunk::Chunk():
+mPosition(0),
+mDataSize(0),
+mDataPosition(0),
+mId(NULL)
+{
+}
+
+nuiChunkSampleReader::Chunk::Chunk(nglFileOffset position, nglFileOffset dataSize, nglFileOffset dataPosition, const char* Id):
+mPosition(position),
+mDataSize(dataSize),
+mDataPosition(dataPosition),
+mId(NULL)
+{
+  if (Id)
+  {
+    mId = new char[strlen(Id) + 1];
+    char* res = strcpy(mId, Id);
+    int i = 0;
+  }
+}
+
+nuiChunkSampleReader::Chunk::~Chunk()
+{
+  if (mId)
+  {
+    delete[] mId;
+    mId = NULL;
+  }
+}
+
+
+bool nuiChunkSampleReader::Chunk::CompareId(const char* Id) const
+{
+  const int compare = strcmp(mId, Id);
+  return (compare == 0);
+}
+
