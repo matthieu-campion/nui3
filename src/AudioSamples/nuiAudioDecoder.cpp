da52d4852d9d8d389772e188d906a4856a8e06c0
mat <mat@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2009-01-21 15:36:41 +0000

    more work on nuiAudioDecoder
    git-svn-id: svn://libnui.net/nui/trunk/nui3@4254 9db30882-7939-4ce0-a36a-45f8a15c01b5

--- src/AudioSamples/nuiAudioDecoder.cpp
+++ src/AudioSamples/nuiAudioDecoder.cpp
@@ -8,6 +8,7 @@
  */
 #include "nui.h"
 #include "nuiAudioDecoder.h"
+#include "nuiAudioConvert.h"
 
 nuiAudioDecoder::nuiAudioDecoder(nglIStream& rStream) :
   nuiSampleReader(rStream),
@@ -44,6 +45,51 @@ void nuiAudioDecoder::SetPosition(uint32 pos)
   {
     mPosition = pos;
   }
+}
+
+
+uint32 nuiAudioDecoder::ReadIN(void* pBuffer, uint32 sampleframes, nuiSampleBitFormat format)
+{
+  //don't increment mPosition: it's already done in ReadDE
+  uint32 channels = mInfo.GetChannels();
+  std::vector<float*> temp(channels);
+  std::vector<void*> tempVoid(channels);
+  for (uint32 c= 0; c < channels; c++)
+  {
+    temp[c] = new float[sampleframes];
+    tempVoid[c] = (void*)(temp[c]);
+  }
+  
+  uint32 sampleFramesRead = ReadDE(tempVoid, sampleframes, eSampleFloat32);
+  if (format == eSampleFloat32)
+  {
+    float* pFloatBuffer = (float*)pBuffer;
+    //just interleave samples
+    for (uint32 c = 0; c < channels; c++)
+    {
+      for (uint32 s = 0; s < sampleFramesRead; s++)
+      {
+        pFloatBuffer[s * channels + c] = temp[c][s];
+      }
+    }
+  }
+  else
+  {
+    //16 bits int are required, so interleave samples and convert them into float
+    int16* pInt16Buffer = (int16*)pBuffer;
+    for (uint32 c = 0; c < channels; c++)
+    {
+      nuiAudioConvert_DEfloatToINint16(temp[c], pInt16Buffer, c, channels, sampleFramesRead);
+    }
+    
+  }
+  
+  for (uint32 c= 0; c < channels; c++)
+  {
+    delete[] temp[c];
+  }
+  
+  return sampleFramesRead;
 }
 
 
