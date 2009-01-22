49b20554ce49f4ededeed18314439685f665fb93
meeloo <meeloo@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2009-01-22 16:52:59 +0000

    fixed win32 build in vs 2008 (about to switch developement to vs2008 as soon as all the project build and run ok)
    git-svn-id: svn://libnui.net/nui/trunk/nui3@4259 9db30882-7939-4ce0-a36a-45f8a15c01b5

--- src/Audio/DirectSound/nuiAudioDevice_DirectSound.cpp
+++ src/Audio/DirectSound/nuiAudioDevice_DirectSound.cpp
@@ -1328,4 +1328,4 @@ BOOL CALLBACK nuiAudioDeviceAPI_DirectSound::DSEnumOutputCallback(LPGUID lpGuid,
   return pAPI->RealDSEnumOutputCallback(lpGuid, lpcstrDescription, lpcstrModule);
 }
 
- nuiAudioDeviceAPI_DirectSound DirectSoundApi;
+ nuiAudioDeviceAPI_DirectSound DirectSoundAPI;
