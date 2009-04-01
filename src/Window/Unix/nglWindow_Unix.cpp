91019e946c2498b6aef3a3ee8ce99cf8b330186b
jbl2024 <jbl2024@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2009-04-01 12:44:28 +0000

    added dummy code for the linux build
    git-svn-id: svn://libnui.net/nui/trunk/nui3@4468 9db30882-7939-4ce0-a36a-45f8a15c01b5

--- src/Window/Unix/nglWindow_Unix.cpp
+++ src/Window/Unix/nglWindow_Unix.cpp
@@ -665,36 +665,20 @@ bool nglWindow::IsKeyDown (nglKeyCode Key) const
 /*
  * Drag and drop
  */
+void nglWindow::OnDragEnter() {}
+void nglWindow::OnDragLeave() {}
+bool nglWindow::OnCanDrop(nglDragAndDrop* pDragObject, int X, int Y, nglMouseInfo::Flags Button) { return false; }
+void nglWindow::OnDropped (nglDragAndDrop* pDragObject, int X,int Y, nglMouseInfo::Flags Button) {}
+bool nglWindow::Drag(nglDragAndDrop* pDragObject) { return false; }
 
-void nglWindow::OnDragEnter()
+void nglWindow::OnDragRequestData(nglDragAndDrop* pDragObject, const nglString& rMimeType)
 {
 }
-
-void nglWindow::OnDragLeave()
+void nglWindow::OnDragStop(bool canceled)
 {
 }
+ 
 
-bool nglWindow::OnCanDrop(nglDragAndDrop* pDragObject, int X, int Y, nglMouseInfo::Flags Button)
-{
-  return false;
-}
-
-void nglWindow::OnDropped (nglDragAndDrop* pDragObject, int X,int Y, nglMouseInfo::Flags Button)
-{
-}
-
-bool nglWindow::Drag(nglDragAndDrop* pDragObject)
-{
-  return false;
-}
-
-void nglWindow::OnDragged (nglDragAndDrop* pDragObject)
-{
-}
-
-void nglWindow::OnStopDragging()
-{
-}
 
 
 /*
