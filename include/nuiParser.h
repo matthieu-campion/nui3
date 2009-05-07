aac3065c6071de1fcf5daf58e4f22e2a8ec9e0da
meeloo <meeloo@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2009-05-07 12:40:55 +0000

    added nuiParser, a simple parser helper class that is easy to build text reading classes on
    git-svn-id: svn://libnui.net/nui/trunk/nui3@4567 9db30882-7939-4ce0-a36a-45f8a15c01b5

---
+++ include/nuiParser.h
@@ -0,0 +1,63 @@
+/*
+ NUI3 - C++ cross-platform GUI framework for OpenGL based applications
+ Copyright (C) 2002-2003 Sebastien Metrot & Vincent Caron
+ 
+ licence: see nui3/LICENCE.TXT
+*/
+
+class nuiParser
+{
+public:
+  nuiParser(nglIStream* pStream, const nglPath& rSourcePath);
+
+  bool PeekChar(); ///< Read the next char but don't advance the read index.
+  bool NextChar(); ///< Read the next char and advance the read index.
+  nglChar GetChar() const; ///< Get the current char.
+
+  bool SkipBlank(); ///< Advance the read pointer until the current char is not a blank.  Returns false is there is a problem.
+  bool GetQuoted(nglString& rResult); ///< Read a Quoted string. Returns false is there is a problem.
+  bool GetSymbol(nglString& rResult); ///< Read a symbol. Returns false is there is a problem.
+  bool GetValue(nglString& rResult, bool AllowBlank = false); ///< Read a value. Returns false is there is a problem.
+  bool GetFloat(float& rResult); ///< Read a float. Returns false is there is a problem.
+  bool GetFloat(double& rResult); ///< Read a float. Returns false is there is a problem.
+  bool GetInteger(uint8&  rResult, uint8 Base = 10); ///< Read an integer. Returns false is there is a problem.
+  bool GetInteger(uint16& rResult, uint8 Base = 10); ///< Read an integer. Returns false is there is a problem.
+  bool GetInteger(uint32& rResult, uint8 Base = 10); ///< Read an integer. Returns false is there is a problem.
+  bool GetInteger(uint64& rResult, uint8 Base = 10); ///< Read an integer. Returns false is there is a problem.
+  bool GetInteger(int8&   rResult, uint8 Base = 10); ///< Read an integer. Returns false is there is a problem.
+  bool GetInteger(int16&  rResult, uint8 Base = 10); ///< Read an integer. Returns false is there is a problem.
+  bool GetInteger(int32&  rResult, uint8 Base = 10); ///< Read an integer. Returns false is there is a problem.
+  bool GetInteger(int64&  rResult, uint8 Base = 10); ///< Read an integer. Returns false is there is a problem.
+  
+  void SetError(const nglString& rError); ///< Change the error message.
+  int32 GetLine() const; ///< Return the current line number
+  int32 GetColumn() const; ///< Return the current column.
+  const nglString& GetErrorStr() const; ///< Return the error string stored in the parser.
+  
+  void SetValidInSymbolStart(const nglString& rValidChars); ///< Set the characters that are valid as the first character of a symbol (usualy all latin letters + underscore).
+  void SetValidInSymbol(const nglString& rValidChars); ///< Set the characters that are valid in a symbol (usualy all latin characters + underscore + latin numbers).
+  void SetValidInValue(const nglString& rValidChars); ///< Set the characters that are valid in a value.
+  void SetValidInBlank(const nglString& rValidChars); ///< Set the characters that are considered blanks.
+
+  bool IsValidInSymbolStart(nglChar c) const; ///< Returns true if the given char is valid as a symbol start
+  bool IsValidInSymbol(nglChar c) const; ///< Returns true if the given char is valid in a symbol
+  bool IsValidInValue(nglChar c) const; // Returns true is the given char is valid in a value
+  bool IsBlank(nglChar c) const; ///< Returns true if the given char is a blank.
+  bool IsNumberDigit(nglChar c, uint32 Base = 10) const; ///< Returns true if the given char is a valid number digit for the given base.
+  bool GetNumberDigit(uint8& res, nglChar c, uint32 Base) const; ///< Returns true if the given char is a valid number digit for the given base. In this case res contains the converted digit as a number.
+
+protected:
+  nglIStream* mpStream;
+  nglPath mSourcePath;
+  nglChar mChar;
+  int32 mColumn;
+  int32 mLine;
+  bool mEnableCppComments;
+  bool mError;
+  nglString mErrorString;
+  std::set<nglChar> mValidInSymbolStart;
+  std::set<nglChar> mValidInSymbol;
+  std::set<nglChar> mValidInValue;
+  std::set<nglChar> mLineCommentStarters;
+  std::set<nglChar> mBlanks;
+};
---
+++ src/Utils/nuiParser.cpp
@@ -0,0 +1,447 @@
+/*
+ NUI3 - C++ cross-platform GUI framework for OpenGL based applications
+ Copyright (C) 2002-2003 Sebastien Metrot & Vincent Caron
+ 
+ licence: see nui3/LICENCE.TXT
+ */
+
+#include "nui.h"
+#include "nuiParser.h"
+
+
+//class nuiParser
+
+nuiParser::nuiParser(nglIStream* pStream, const nglPath& rSourcePath)
+{
+  mpStream = pStream;
+  mSourcePath = rSourcePath;
+  if (mSourcePath.IsLeaf())
+    mSourcePath = mSourcePath.GetParent();
+  mChar = _T(' ');
+  mColumn = 1;
+  mLine = 1;
+  
+  SetValidInValue(_T("!@#$%<>*?'+-&~|[]{}\().,"));
+  SetValidInSymbolStart(_T("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_"));
+  SetValidInSymbol(_T("0123456789"));
+  SetValidInBlank(_T(" \t\r\n"));
+}
+
+int32 nuiParser::GetLine() const
+{
+  return mLine;
+}
+
+int32 nuiParser::GetColumn() const
+{
+  return mColumn;
+}
+
+const nglString& nuiParser::GetErrorStr() const
+{
+  return mErrorString;
+}
+
+bool nuiParser::PeekChar()
+{
+  nglFileOffset ofs = mpStream->GetPos();
+  bool res = GetChar();
+  mpStream->SetPos(ofs);
+  return res;
+}
+
+bool nuiParser::NextChar()
+{
+  nglChar previous = mChar;
+  // Parse an utf-8 char sequence:
+  uint8 c = 0;
+  if (1 != mpStream->ReadUInt8(&c, 1))
+  {
+    mChar = 0;
+    return false;
+  }
+  if (!(c & 0x80))
+  {
+    mChar = c;
+  }
+  else
+  {
+    //  0xC0 // 2 bytes
+    //  0xE0 // 3
+    //  0xF0 // 4
+    //  0xF8 // 5
+    //  0xFC // 6
+    uint32 count = 0;
+    if (c & 0xFC == 0xFC)
+    {
+      mChar = c & ~0xFC;
+      count = 5;
+    }
+    else if (c & 0xF8 == 0xF8)
+    {
+      mChar = c & ~0xF8;
+      count = 4;
+    }
+    else if (c & 0xF0 == 0xF0)
+    {
+      mChar = c & ~0xF0;
+      count = 3;
+    }
+    else if (c & 0xE0 == 0xE0)
+    {
+      mChar = c & ~0xE0;
+      count = 2;
+    }
+    else if (c & 0xC0 == 0xC0)
+    {
+      mChar = c & ~0xC0;
+      count = 1;
+    }
+    
+    for (uint32 i = 0; i < count; i++)
+    {
+      if (1 != mpStream->ReadUInt8(&c, 1))
+        return false;
+      mChar <<= 6;
+      mChar |= c & 0x3F;
+    }
+  }
+  
+  if ((mChar == 0xa && previous != 0xd) || (mChar == 0xd && previous != 0xa) )
+  {
+    mColumn = 1;
+    mLine++;
+  }
+  
+  //NGL_OUT(_T("%lc"), mChar);
+  
+  return true;
+}
+
+nglChar nuiParser::GetChar() const
+{
+  return mChar;
+}
+
+bool nuiParser::IsBlank(nglChar c) const
+{
+  return mBlanks.find(c) != mBlanks.end();
+}
+
+bool nuiParser::SkipBlank()
+{
+  bool res = true;
+  while ((IsBlank(mChar) || mChar == '/') && res) 
+  {
+    if (mChar == '/') // Is this a comment?
+    {
+      res = PeekChar();
+      
+      if (!res)
+        return false;
+      
+      if (mChar == '/')
+      {
+        res = GetChar();
+        // Skip to the end of the line:
+        res = GetChar();
+        while (res && mChar != 0xa && mChar != 0xd)
+        {
+          res = GetChar();
+        }
+      }
+    }
+    
+    res = GetChar();
+  }
+  return res;
+}
+
+bool nuiParser::GetQuoted(nglString& rResult)
+{
+  if (!SkipBlank())
+    return false;
+  
+  if (mChar != _T('\"'))
+    return false;
+  
+  while (GetChar() && mChar != _T('\"'))
+  {
+    if (mChar == _T('\\'))
+    {
+      if (!GetChar())
+        return false;
+      
+      if (mChar != _T('\"'))
+        return false;
+      
+    }
+    
+    rResult.Add(mChar);
+  }
+  
+  GetChar();
+  return true;
+}
+
+bool nuiParser::GetSymbol(nglString& rResult)
+{
+  rResult.Nullify();
+  if (!SkipBlank())
+    return false;
+  
+  while (IsValidInSymbol(mChar))
+  {
+    rResult.Add(mChar);
+    GetChar();
+  }
+  
+  return !rResult.IsEmpty();
+}
+
+bool nuiParser::GetValue(nglString& rResult, bool AllowBlank)
+{
+  rResult.Nullify();
+  if (!SkipBlank())
+    return false;
+  
+  while ((AllowBlank && IsBlank(mChar)) || IsValidInValue(mChar))
+  {
+    rResult.Add(mChar);
+    GetChar();
+  }
+  
+  return true;
+}
+
+void nuiParser::SetError(const nglString& rError)
+{
+  mErrorString = rError;
+  mError = true;
+}
+
+bool nuiParser::GetFloat(float& rResult)
+{
+  double r;
+  bool res = GetFloat(r);
+  rResult = r;
+  return res;
+}
+
+bool nuiParser::GetFloat(double& rResult)
+{
+  bool neg = false;
+  double value = 0;
+  //  bool dotfound = false;
+  
+  if (mChar == '-')
+  {
+    neg = true;
+    if (!NextChar())
+      return false;
+  }
+  
+  uint8 c = 0;
+  if (!GetNumberDigit(c, mChar, 10))
+    return false;
+  while (mChar && c >= 0 && c <= 9)
+  {
+    value = value * 10 + c;
+    if (!NextChar())
+      return false;
+    if (!GetNumberDigit(c, mChar, 10))
+      return false;
+  }
+  
+  // Read decimal dot:
+  if (mChar == '.')
+  {
+    if (!NextChar())
+      return false;
+    double count = 1, v = 0;
+    if (!GetNumberDigit(c, mChar, 10))
+      return false;
+    while (mChar && c >= 0 && c <= 9) // Read decimal part:
+    {
+      v = v * 10 + c;
+      count *= 10;
+      if (!NextChar())
+        return false;
+      if (!GetNumberDigit(c, mChar, 10))
+        return false;
+    }
+    
+    if (count)
+    {
+      value += v / count;
+    }
+  }
+  
+  if (mChar == 'e' || mChar == 'E')
+  {
+    if (!NextChar())
+      return false;
+    int32 exponent;
+    if (!GetInteger(exponent, 10))
+      return false;
+    value *= pow(10.0, exponent);
+  }
+  
+  return neg ? -value : value;
+  
+}
+
+
+bool nuiParser::GetInteger(uint8&  rResult, uint8 Base)
+{
+  uint64 r;
+  bool res = GetInteger(r, Base);
+  rResult = r;
+  return res & (r < (1 << 8));
+}
+
+bool nuiParser::GetInteger(uint16& rResult, uint8 Base)
+{
+  uint64 r;
+  bool res = GetInteger(r, Base);
+  rResult = r;
+  return res & (r < (1 << 16));
+}
+
+bool nuiParser::GetInteger(uint32& rResult, uint8 Base)
+{
+  uint64 r;
+  bool res = GetInteger(r, Base);
+  rResult = r;
+  return res & (r < (1 << 32));
+}
+
+bool nuiParser::GetInteger(uint64& rResult, uint8 Base)
+{
+  uint64 old = 0;
+  rResult = 0;
+  uint8 digit = 0;
+  
+  while (GetNumberDigit(digit, mChar, Base))
+  {
+    rResult *= Base;
+    rResult += digit;
+    
+    if ((rResult - digit) != (old * Base))
+      return false;
+  }
+  return true;
+}
+
+bool nuiParser::GetInteger(int8&  rResult, uint8 Base)
+{
+  uint64 r;
+  bool res = GetInteger(r, Base);
+  rResult = r;
+  return res & (r < (1 << 8));
+}
+
+bool nuiParser::GetInteger(int16& rResult, uint8 Base)
+{
+  uint64 r;
+  bool res = GetInteger(r, Base);
+  rResult = r;
+  return res & (r < (1 << 16));
+}
+
+bool nuiParser::GetInteger(int32& rResult, uint8 Base)
+{
+  uint64 r;
+  bool res = GetInteger(r, Base);
+  rResult = r;
+  return res & (r < (1 << 32));
+}
+
+bool nuiParser::GetInteger(int64& rResult, uint8 Base)
+{
+  int64 old = 0;
+  rResult = 0;
+  uint8 digit = 0;
+  bool neg = false;
+  if (mChar == '-')
+  {
+    neg = true;
+    NextChar();
+  }
+  
+  while (GetNumberDigit(digit, mChar, Base))
+  {
+    rResult *= Base;
+    rResult += digit;
+    
+    if ((rResult - digit) != (old * Base))
+      return false;
+  }
+  
+  if (neg)
+    rResult = -rResult;
+  return true;
+}
+
+
+void nuiParser::SetValidInSymbolStart(const nglString& rValidChars)
+{
+  for (uint32 i = 0; i < rValidChars.GetLength(); i++)
+    mValidInSymbolStart.insert(rValidChars[i]);
+}
+
+void nuiParser::SetValidInSymbol(const nglString& rValidChars)
+{
+  for (uint32 i = 0; i < rValidChars.GetLength(); i++)
+    mValidInSymbol.insert(rValidChars[i]);
+}
+
+void nuiParser::SetValidInValue(const nglString& rValidChars)
+{
+  for (uint32 i = 0; i < rValidChars.GetLength(); i++)
+    mValidInValue.insert(rValidChars[i]);
+}
+
+bool nuiParser::IsValidInValue(nglChar c) const
+{
+  if (mValidInValue.find(c) != mValidInValue.end())
+    return true;
+  return IsValidInSymbol(c);
+}
+
+void nuiParser::SetValidInBlank(const nglString& rValidChars)
+{
+  for (uint32 i = 0; i < rValidChars.GetLength(); i++)
+    mBlanks.insert(rValidChars[i]);
+}
+
+bool nuiParser::IsValidInSymbolStart(nglChar c) const
+{
+  if (mValidInSymbolStart.find(c) != mValidInSymbolStart.end())
+    return true;
+  return IsValidInSymbol(c);
+}
+
+bool nuiParser::IsNumberDigit(nglChar c, uint32 Base) const
+{
+  uint8 d = 0;
+  return GetNumberDigit(d, c, Base);
+}
+
+bool nuiParser::GetNumberDigit(uint8& res, nglChar c, uint32 Base) const
+{
+  if (c >= '0' && c <= '9')
+    c -= '0';
+  else if (c >= 'a' && c <= 'z')
+  {
+    c -= 'a';
+    c += 10;
+  }
+  else if (c >= 'A' && c <= 'Z')
+  {
+    c -= 'a';
+    c += 10;
+  }
+  res = c;
+  return c < Base;
+}
