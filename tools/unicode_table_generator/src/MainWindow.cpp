baeab755fc0672a743e0b7dfb3dc8520b54c3575
meeloo <meeloo@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2009-05-11 00:51:45 +0000

    added script testing to unicode tools
    git-svn-id: svn://libnui.net/nui/trunk/nui3@4573 9db30882-7939-4ce0-a36a-45f8a15c01b5

--- tools/unicode_table_generator/src/MainWindow.cpp
+++ tools/unicode_table_generator/src/MainWindow.cpp
@@ -62,6 +62,7 @@ MainWindow::MainWindow(const nglContextInfo& rContextInfo, const nglWindowInfo& 
   nuiParser parser(&stream, url);
   
   std::list<Range> Ranges;
+  std::set<nglString> Scripts;
   
 #define ERROR(x) { mpText->AddText(x); mpText->AddText(_T("\n")); NGL_OUT(x); NGL_ASSERT(0); return; }
   parser.NextChar();
@@ -115,12 +116,27 @@ MainWindow::MainWindow(const nglContextInfo& rContextInfo, const nglWindowInfo& 
       
       //NGL_OUT(_T("found %ls %04x..%04x\n"), script.GetChars(), low, high);
       Ranges.push_back(Range(script, low, high));
+      Scripts.insert(script);
       
       if (!parser.SkipToNextLine())
         ERROR(_T("parser error skipping to next line"));
 
     }
   }
+
+  std::set<nglString>::iterator sit = Scripts.begin();
+  std::set<nglString>::iterator send = Scripts.end();
+
+  NGL_OUT(_T("enum nuiUnicodeScript\n{\n"));
+  while (sit != send)
+  {
+    nglString s(*sit);
+    
+    NGL_OUT(_T("  eScript%ls,\n"), s.GetChars());
+
+    ++sit;
+  }
+  NGL_OUT(_T("};\n\n"));
   
   Ranges.sort(RangeLess);
 
@@ -150,77 +166,19 @@ MainWindow::MainWindow(const nglContextInfo& rContextInfo, const nglWindowInfo& 
     ++it;
   }
   
+  
+  NGL_OUT(_T("class nuiScriptRange\n{\npublic:\n  nuiUnicodeScript mScript;\n  int32 mLow;\n  int32 mHigh;\n};\n\n"));
+  
+  NGL_OUT(_T("nuiScriptRange nuiScriptRanges[] =\n{\n"));
   it = miniranges.begin();
   end = miniranges.end();
   while (it != end)
   {
     Range& r(*it);
-    NGL_OUT(_T("found %20ls\t\t %04x..%04x\n"), r.mName.GetChars(), r.mLow, r.mHi);
+    NGL_OUT(_T("  { eScript%ls, %d, %d },\n"), r.mName.GetChars(), r.mLow, r.mHi);
     ++it;
   }    
-/*  
-  std::map<nglString, std::vector< std::pair<nglChar, nglChar> > > scripts;
-  const std::vector<std::vector<nglString> >& rDoc(csv.GetDocument());
-
-  for (uint32 i = 0; i < rDoc.size(); i++)
-  {
-    nglChar low = 0;
-    nglChar high = 0;
-    nglString script;
-    const std::vector<nglString>& rLine(rDoc[i]);
-    if (rLine.size() == 2)
-    {
-      // double first entry
-      low = nglString(rLine[0].Extract(2)).GetCInt(16);
-      high = low;
-      script = rLine[1];
-    }
-    else if (rLine.size() == 3)
-    {
-      // complete line
-      low = nglString(rLine[0].Extract(2)).GetCInt(16);
-      high = nglString(rLine[1].Extract(2)).GetCInt(16);
-      script = rLine[2];
-    }
-    
-    if (scripts[script].empty())
-      scripts[script].push_back(std::make_pair(low, high));
-    else
-    {
-      std::vector< std::pair<nglChar, nglChar> >& rScript(scripts[script]);
-      uint32 s = rScript.size() - 1;
-      if (rScript[s].second + 1 == low)
-        rScript[s].second = high;
-      else
-        rScript.push_back(std::make_pair(low, high));
-    }
-    
-//    nglString str;
-//    str.CFormat(_T("%ls\t%d - %d\n"), script.GetChars(), low, high);
-//    mpText->AddText(str);
-                    
-  }
-
-  
-  std::map<nglString, std::vector< std::pair<nglChar, nglChar> > >::iterator it = scripts.begin();
-  std::map<nglString, std::vector< std::pair<nglChar, nglChar> > >::iterator end = scripts.end();
-  while (it != end)
-  {
-    const std::vector< std::pair<nglChar, nglChar> >& rScript(it->second);
-    nglString script(it->first);
-    nglString str;
-    for (uint32 i = 0; i < rScript.size(); i++)
-    {
-      nglChar low = rScript[i].first;
-      nglChar high = rScript[i].second;
-      str.CFormat(_T("%ls\t%d - %d\n"), script.GetChars(), low, high);
-      mpText->AddText(str);
-      NGL_OUT(str.GetChars());
-    }
-    
-    ++it;
-  }
-  */
+  NGL_OUT(_T("};\n\n"));
 }
 
 MainWindow::~MainWindow()
