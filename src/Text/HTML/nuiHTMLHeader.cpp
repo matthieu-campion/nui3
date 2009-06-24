2dca55b49dd437dfe1881998d1329db2042add87
meeloo <meeloo@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2009-06-24 14:14:00 +0000

    forgot to add nuiHTMLHeader.*
    git-svn-id: svn://libnui.net/nui/trunk/nui3@4660 9db30882-7939-4ce0-a36a-45f8a15c01b5

---
+++ src/Text/HTML/nuiHTMLHeader.cpp
@@ -0,0 +1,55 @@
+/*
+ NUI3 - C++ cross-platform GUI framework for OpenGL based applications
+ Copyright (C) 2002-2003 Sebastien Metrot
+ 
+ licence: see nui3/LICENCE.TXT
+ */
+
+#include "nui.h"
+#include "nuiHTMLHeader.h"
+
+// class nuiHTMLHeader : public nuiHTMLBox
+nuiHTMLHeader::nuiHTMLHeader(nuiHTMLNode* pNode)
+: nuiHTMLBox(pNode, false)
+{
+}
+
+nuiHTMLHeader::~nuiHTMLHeader()
+{
+}
+
+void nuiHTMLHeader::Layout(nuiHTMLContext& rContext)
+{
+  PushContext(rContext);
+
+  float s = 12;
+  switch (mpNode->GetTagType())
+  {
+  case nuiHTML::eTag_H1:
+    s = 20;
+    break;
+  case nuiHTML::eTag_H2:
+    s = 18;
+    break;
+  case nuiHTML::eTag_H3:
+    s = 16;
+    break;
+  case nuiHTML::eTag_H4:
+    s = 14;
+    break;
+  case nuiHTML::eTag_H5:
+    s = 13;
+    break;
+  case nuiHTML::eTag_H6:
+    s = 12;
+    break;
+  }
+  
+  rContext.mFont.ClearMustHaveSize();
+  rContext.mFont.MustHaveSize(s, 1.0f);
+  
+  nuiHTMLBox::Layout(rContext);
+  
+  PopContext(rContext);
+}
+
---
+++ src/Text/HTML/nuiHTMLHeader.h
@@ -0,0 +1,22 @@
+/*
+ NUI3 - C++ cross-platform GUI framework for OpenGL based applications
+ Copyright (C) 2002-2003 Sebastien Metrot
+ 
+ licence: see nui3/LICENCE.TXT
+ */
+
+#pragma once
+
+#include "nui.h"
+#include "nuiHTMLBox.h"
+
+class nuiHTMLHeader : public nuiHTMLBox
+{
+public:
+  nuiHTMLHeader(nuiHTMLNode* pNode);
+  virtual ~nuiHTMLHeader();
+  
+  virtual void Layout(nuiHTMLContext& rContext);
+  
+};
+
