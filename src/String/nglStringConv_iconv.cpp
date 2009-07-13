8dc0bb4e309b94ee8d49fda2027cef8115a4242a
meeloo <meeloo@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2009-07-13 15:42:00 +0000

    damn iconv
    git-svn-id: svn://libnui.net/nui/trunk/nui3@4693 9db30882-7939-4ce0-a36a-45f8a15c01b5

--- src/String/nglStringConv_iconv.cpp
+++ src/String/nglStringConv_iconv.cpp
@@ -41,7 +41,7 @@ nglStringConv::~nglStringConv()
 
 int nglStringConv::Process (const char*& pSource, int& rToRead, char*& pTarget, int& rToWrite)
 {
-#ifdef __UIKIT__
+#ifdef _UIKIT_
   char* in = const_cast<char*>(pSource);
 #else
   const char* in = pSource;
