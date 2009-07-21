f4ba8824235a03d815e2101e7feb23ac3baec9e6
jbl2024 <jbl2024@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2009-07-21 15:25:56 +0000

    first implementation of nuiHTTP under linux (using libcurl) warning : experimental (ie not fully tested :) )
    git-svn-id: svn://libnui.net/nui/trunk/nui3@4701 9db30882-7939-4ce0-a36a-45f8a15c01b5

--- src/Net/Unix/nuiHTTP_Unix.cpp
+++ src/Net/Unix/nuiHTTP_Unix.cpp
@@ -20,7 +20,115 @@
 #include "nui.h"
 #include "nuiHTTP.h"
 
+#include <curl/curl.h>
+using namespace std;
+static size_t write_data(char *data, size_t size, size_t nmemb,
+                            nglString *buffer)
+{  
+  for(int i = 0; i < size*nmemb; i++)
+  {
+    if (data[i] > 0)
+      buffer->Append(nglChar(data[i]));
+  }
+  return size * nmemb;
+}
+
+
 nuiHTTPResponse* nuiHTTPRequest::SendRequest()
 {
-  return NULL;
+  // parse address string
+  nglString url(mUrl);
+  nglString server, objectName;
+  nglString buffer;
+  nglString header;
+  uint port = 80;
+  CURLcode res;
+
+  NGL_OUT(_T("%s\n"), __FILE__);
+  
+  if (url.CompareLeft(_T("http://")) == 0)
+  {
+    url.DeleteLeft(7);
+  }
+
+  int32 pos = url.Find(':');
+  if (pos != -1)
+  {
+    server = url.Extract(0, pos);
+    port = url.Extract(pos).GetCInt();
+    pos = url.Find('/');
+    if (pos != -1)
+    {
+      objectName = url.Extract(pos);
+    }
+    else
+    {
+      objectName = nglString::Null;
+    }
+  }
+  else
+  {
+    pos = url.Find('/');
+    if (pos != -1)
+    {
+      server = url.Extract(0, pos);
+      objectName = url.Extract(pos);
+    }
+    else
+    {
+      server = url;
+      objectName = nglString::Null;
+    }
+  }
+
+  CURL *easyhandle = curl_easy_init();
+  if (mMethod == _T("POST"))
+  {
+    res = curl_easy_setopt(easyhandle, CURLOPT_URL, url.GetStdString().c_str());
+    res = curl_easy_setopt(easyhandle, CURLOPT_POSTFIELDS, objectName.GetStdString().c_str());
+  }
+  else if (mMethod == _T("GET"))
+  {
+    NGL_OUT(_T("url = %ls\n"), mUrl.GetChars());
+    res = curl_easy_setopt(easyhandle, CURLOPT_URL, mUrl.GetStdString().c_str());
+  }
+
+
+  curl_easy_setopt(easyhandle, CURLOPT_WRITEFUNCTION, write_data);
+  curl_easy_setopt(easyhandle, CURLOPT_WRITEDATA, &buffer);
+  curl_easy_setopt(easyhandle, CURLOPT_WRITEHEADER, &header);
+  curl_easy_perform(easyhandle);
+
+  long statusCode = 0;
+  curl_easy_getinfo (easyhandle, CURLINFO_RESPONSE_CODE, &statusCode);
+  vector<nglString> headersVec;
+  header.Tokenize(headersVec, _T("\r\n"));
+
+  nglString statusLine;
+  
+  vector<nglString>::const_iterator it = headersVec.begin();
+  vector<nglString>::const_iterator end = headersVec.end();
+  if (it != end)
+  {
+    statusLine = *it;
+    ++it;
+  }
+
+  nuiHTTPResponse* pResponse = new nuiHTTPResponse((uint16) statusCode, statusLine);
+
+  for (; it != end; ++it)
+  {
+    pos = it->Find(_T(":"));
+    nglString fieldName(it->Extract(0, pos));
+    fieldName.Trim();
+    nglString fieldValue(it->Extract(pos+1));
+    fieldValue.Trim();
+    pResponse->AddHeader(fieldName, fieldValue);
+  }
+
+  //NGL_OUT(_T("body = %s\n"), buffer.GetStdString().c_str()); 
+  pResponse->SetBody(buffer.GetStdString().c_str(), buffer.GetLength());
+  
+  curl_easy_cleanup(easyhandle);
+  return pResponse;
 }
