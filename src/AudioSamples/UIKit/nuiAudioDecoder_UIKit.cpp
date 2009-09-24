c763c7a8df76480bcef91e0b03bc89e1299981b9
mat <mat@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2009-09-24 08:22:05 +0000

    improved nuiAudioDecoder for iPhone (almost OK)
    git-svn-id: svn://libnui.net/nui/trunk/nui3@4805 9db30882-7939-4ce0-a36a-45f8a15c01b5

--- src/AudioSamples/UIKit/nuiAudioDecoder_UIKit.cpp
+++ src/AudioSamples/UIKit/nuiAudioDecoder_UIKit.cpp
@@ -8,6 +8,7 @@
  */
 
 #include "nuiAudioDecoder.h"
+#include "nuiAudioConvert.h"
 
 #include <AudioToolBox/AudioToolBox.h>
 #include "AudioToolBox/ExtendedAudioFile.h"
@@ -15,9 +16,12 @@
 OSStatus MyAudioFile_ReadProc(void* pInClientData, SInt64 inPosition, UInt32 requestCount, void* pBuffer, UInt32* pActualCount)
 {	
 	nglIStream* pStream = (nglIStream*)pInClientData;
-	
+  
+  uint32 avail = pStream->Available();
 	pStream->SetPos(inPosition);
 	
+  avail = pStream->Available();
+  
 	uint8* pOut = (uint8*)pBuffer;
 	*pActualCount = pStream->ReadUInt8(pOut, requestCount);
 	
@@ -27,6 +31,7 @@ OSStatus MyAudioFile_ReadProc(void* pInClientData, SInt64 inPosition, UInt32 req
 OSStatus MyAudioFile_WriteProc (void* pInClientData, SInt64 inPosition,	UInt32 requestCount, const void* pBuffer,	UInt32* pActualCount)
 {
 	NGL_ASSERT(0);
+  printf("WriteProc\n");
 	*pActualCount = 0;
 	return noErr;
 }
@@ -43,9 +48,16 @@ SInt64 MyAudioFile_GetSizeProc(void* pInClientData)
 OSStatus MyAudioFile_SetSizeProc(void* pInClientData, SInt64 inSize)
 {
 	NGL_ASSERT(0);
+  printf("SetSizeProc\n");
 	return noErr;
 }
 
+
+//
+//
+// nuiAudioDecoderPrivate
+//
+//
 
 class nuiAudioDecoderPrivate
   {
@@ -94,6 +106,15 @@ void nuiAudioDecoderPrivate::Clear()
 		AudioFileClose(mAudioFileID);
 }
 
+
+
+
+//
+//
+// nuiAudioDecoder
+//
+//
+
 void nuiAudioDecoder::Clear()
 {
   if (mpPrivate)
@@ -117,27 +138,108 @@ bool nuiAudioDecoder::ReadInfo()
 {
   NGL_ASSERT(mpPrivate);
   
-  AudioStreamBasicDescription desc;
+  AudioStreamBasicDescription FileDesc;
+  AudioStreamBasicDescription ClientDesc;
   UInt32 PropDataSize;
-  Boolean PropWritable;
+  OSStatus res;
   
-  OSStatus res = ExtAudioFileGetPropertyInfo(mpPrivate->mExtAudioFileRef, kExtAudioFileProperty_FileDataFormat, &PropDataSize, &PropWritable);
-  
-  res = ExtAudioFileGetProperty(mpPrivate->mExtAudioFileRef, kExtAudioFileProperty_FileDataFormat, &PropDataSize, &desc);
+  PropDataSize = sizeof(FileDesc);
+  res = ExtAudioFileGetProperty(mpPrivate->mExtAudioFileRef, kExtAudioFileProperty_FileDataFormat, &PropDataSize, &FileDesc);
   if (res != noErr)
     return false;
   
-  // todo...
+  double SampleRate = FileDesc.mSampleRate;
+  uint32 channels = FileDesc.mChannelsPerFrame;
+  uint32 BitsPerSample = 32;
+  ClientDesc.mSampleRate = SampleRate;
+  ClientDesc.mChannelsPerFrame = channels;
+  ClientDesc.mFormatFlags = kAudioFormatFlagIsFloat | kAudioFormatFlagIsPacked | kAudioFormatFlagIsNonInterleaved;
+  ClientDesc.mFormatID = kAudioFormatLinearPCM;
+  ClientDesc.mFramesPerPacket = 1;
+  ClientDesc.mBitsPerChannel = BitsPerSample;
+  ClientDesc.mBytesPerFrame = BitsPerSample / 8;
+  ClientDesc.mBytesPerPacket = BitsPerSample / 8;
+
+  PropDataSize = sizeof(ClientDesc);  
+  res = ExtAudioFileSetProperty(mpPrivate->mExtAudioFileRef, kExtAudioFileProperty_ClientDataFormat, PropDataSize, &ClientDesc);
+  if (res != noErr)
+    return false;
+  
+  // get length
+  SInt64 SampleFrames = 0;  
+  PropDataSize = sizeof(SampleFrames);
+  res = ExtAudioFileGetProperty(mpPrivate->mExtAudioFileRef, kExtAudioFileProperty_FileLengthFrames, &PropDataSize, &SampleFrames);
+  if (res != noErr)
+    return false;
+  
+  mInfo.SetSampleFrames(SampleFrames);
+  mInfo.SetSampleRate(SampleRate);
+  mInfo.SetChannels(channels);
+  mInfo.SetBitsPerSample(BitsPerSample);
+  mInfo.SetFileFormat(eAudioCompressed);
+  mInfo.SetStartFrame(0);
+  mInfo.SetStopFrame(mInfo.GetSampleFrames());
   
   return true;
 }
 
 bool nuiAudioDecoder::Seek(uint64 SampleFrame)
 {
-  return true;
+  OSStatus err = ExtAudioFileSeek(mpPrivate->mExtAudioFileRef, mPosition);
+  return (err == noErr);
 }
 
 uint32 nuiAudioDecoder::ReadDE(std::vector<void*> buffers, uint32 sampleframes, nuiSampleBitFormat format)
 {
-  return 0;
+  if (!mInitialized)
+    return 0;
+  
+  uint32 channels = mInfo.GetChannels();
+  if (buffers.size() != channels)
+    return 0;
+  
+  std::vector<float*> temp(channels);
+  for (uint32 c = 0; c < channels; c++)
+  {
+    if (format == eSampleFloat32)
+      temp[c] = (float*)(buffers[c]);
+    else
+      temp[c] = new float[sampleframes];
+  }
+  
+  //
+  uint64 BytesToRead = SampleFramesToBytes(sampleframes);
+  uint32 listSize = sizeof(AudioBufferList) + sizeof(AudioBuffer)* (channels-1);
+  AudioBufferList* pBufList = reinterpret_cast<AudioBufferList*> (new Byte[listSize]);
+  pBufList->mNumberBuffers = channels; // we query non-interleaved samples, so we need as many buffers as channels
+  for (uint32 c = 0; c < pBufList->mNumberBuffers; c++)
+  {
+    // each AudioBuffer represents one channel (non-interleaved samples)
+    pBufList->mBuffers[c].mNumberChannels   = 1;
+    pBufList->mBuffers[c].mDataByteSize     = BytesToRead / channels;
+    pBufList->mBuffers[c].mData             = temp[c];
+  }
+  
+  //
+  UInt32 frames = sampleframes;
+  OSStatus err = ExtAudioFileRead(mpPrivate->mExtAudioFileRef, &frames, pBufList);
+  if (err != noErr)
+    frames = 0;
+  
+  if (format == eSampleInt16)
+  {
+    for (uint32 c = 0; c < channels; c++)
+    {
+      if (err != noErr)
+      {
+        nuiAudioConvert_FloatBufferTo16bits(temp[c], (int16*)(buffers[c]), frames);
+      }
+      delete[] temp[c];
+    }
+  }
+  
+  delete pBufList;
+  
+  mPosition += frames;
+  return frames;
 }
