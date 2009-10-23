5f345df8f6da87524dafc70f93940f6fbe840ae3
neywen <neywen@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2009-10-23 20:28:57 +0000

    cleaning
    git-svn-id: svn://libnui.net/nui/trunk/nui3@4864 9db30882-7939-4ce0-a36a-45f8a15c01b5

--- tools/sqlite-Viewer/src/Sql/SqlRequest.h
+++ tools/sqlite-Viewer/src/Sql/SqlRequest.h
@@ -26,41 +26,12 @@ public:
   bool GetTags(std::vector<nglString>& rTags);
   
   
-  /// for sql reseting
-  bool Drop(bool force = false);
-  
-  /// insert a new entry into the sql base. values must fit to the table tags. see specific Insert methods in overloaded classes, below
-  bool Insert(const std::list<nglString>& rValues);
-
-  
-  /// delete object(s), depending on the selection criteria
-  bool Delete(const std::list<nglString>& rCriteria, const std::list<nglString>& rCriteriaValues);
-  
-  /// ... for 1 selection criteria
-  bool Delete(const nglString& rCriteria, const nglString& rCriteriaValue);
-
-  
-  
   /// select all object(s) from table, store the results in the list (rows) of maps (columns)
   bool Select(std::vector<std::vector<nglString> >& rResults);
-  
-  /// select object(s) from table, depending on the selection criteria, and store the results in the list (rows) of maps (columns)
-  bool Select(std::vector<std::vector<nglString> >& rResults, const std::list<nglString>& rCriteria, const std::list<nglString>& rCriteriaValues);
-  
-  /// ... for the first row only (<=> first result to match the selection criteria)
-  bool Select(std::vector<nglString>& rResults, const std::list<nglString>& rCriteria, const std::list<nglString>& rCriteriaValues);
-  
-  /// ... for the first row only, for a single selection criteria
-  bool Select(std::vector<nglString>& rResults, const nglString& rCriteria, const nglString& rCriteriaValue);
-  
-
   
   
 
 private:
-  
-  /// internal routine to format the criteria part of the request string
-  nglString Where(const std::list<nglString>& rCriteria, const std::list<nglString>& rCriteriaValues);
   
   
   nglString mTable;
