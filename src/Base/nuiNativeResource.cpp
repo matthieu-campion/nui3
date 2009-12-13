7e45e61657baf17a301a331c067309e40f5b32cf
vince <vince@9db30882-7939-4ce0-a36a-45f8a15c01b5>
2009-12-13 17:36:41 +0000

    quick fix for getting the bundle path on iphoneOS
    git-svn-id: svn://libnui.net/nui/trunk/nui3@4982 9db30882-7939-4ce0-a36a-45f8a15c01b5

--- src/Base/nuiNativeResource.cpp
+++ src/Base/nuiNativeResource.cpp
@@ -53,15 +53,18 @@ static CFBundleRef _CFXBundleCreateFromImageName(CFAllocatorRef allocator, const
 #include <dlfcn.h>
 static nglPath GetResourcePath()
 {
+  CFBundleRef bundle = NULL;
+  
+#ifndef _UIKIT_
   Dl_info inf;
   dladdr((void*)GetResourcePath, &inf);
-  
+
   const char* imagename = inf.dli_fname;
   /* get the bundle of a header */
-  CFBundleRef bundle = NULL;
   if (imagename)
     bundle = _CFXBundleCreateFromImageName (NULL, imagename);
-  
+#endif
+
   if (!bundle)
     bundle = CFBundleGetMainBundle();
   
@@ -130,7 +133,7 @@ nuiNativeResource::nuiNativeResource(const nglPath& rPath)
 #if defined _CARBON_ || defined _UIKIT_ || defined _COCOA_ || defined _LINUX_
   nglPath resourcePath(GetResourcePath());
   resourcePath += rPath;
-  
+
   if (resourcePath.Exists())
   {
     mpIStream = resourcePath.OpenRead();
