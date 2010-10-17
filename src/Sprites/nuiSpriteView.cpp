c49a85e991dc51deba8bc4d45df38ac58e377b77
Sébastien Métrot <meeloo@libnui.net>
2010-10-18 01:13:24 +0200

    added empty sprite view

---
+++ src/Sprites/nuiSpriteView.cpp
@@ -0,0 +1,10 @@
+/*
+ NUI3 - C++ cross-platform GUI framework for OpenGL based applications
+ Copyright (C) 2002-2003 Sebastien Metrot & Vincent Caron
+ 
+ licence: see nui3/LICENCE.TXT
+ */
+
+#include "nui.h"
+#include "nuiSpriteView.h"
+
