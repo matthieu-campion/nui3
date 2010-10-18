87907d5243679bc189ec251682aa18a5b65aa008
meeloo <meeloo@meeloo-kubuntu-64bit.(none)>
2010-10-19 01:13:56 +0200

    fixed linux build

--- src/Window/Unix/nuiMainMenu_Unix.cpp
+++ src/Window/Unix/nuiMainMenu_Unix.cpp
@@ -1,17 +1,17 @@
 /*
  NUI - C++ cross-platform GUI framework for OpenGL based applications
  Copyright (C) 2002-2003 SÈbastien MÈtrot
- 
+
  This library is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
  License as published by the Free Software Foundation; either
  version 2.1 of the License, or (at your option) any later version.
- 
+
  This library is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  Lesser General Public License for more details.
- 
+
  You should have received a copy of the GNU Lesser General Public
  License along with this library; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -180,11 +180,9 @@ nuiMainMenu::~nuiMainMenu()
 
 
 // an item has been added in the widget tree. Do the system-side of the operation (add a menu or add a menu item)
-bool nuiMainMenu::OnItemAdded(const nuiEvent& rEvent)
+void nuiMainMenu::OnItemAdded(const nuiEvent& rEvent)
 {
   //FIXME
-
-  return true;
 }
 
 
