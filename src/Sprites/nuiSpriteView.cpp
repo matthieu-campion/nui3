395deba0f5fcefd49d29fd6c65546073c815dda6
neywen <neywen@neywen.net>
2010-11-10 12:25:07 +0100

    wip

--- src/Sprites/nuiSpriteView.cpp
+++ src/Sprites/nuiSpriteView.cpp
@@ -479,8 +479,6 @@ void nuiSprite::Draw(nuiDrawContext* pContext)
   pContext->PushMatrix();
   pContext->MultMatrix(m);
   
-  NGL_OUT(_T("mCurrentAnimation %d   mCurrentFrame %.2f\n"), mCurrentAnimation, mCurrentFrame);
-  
   const nuiSpriteAnimation* pAnim = mpSpriteDef->GetAnimation(mCurrentAnimation);
   const nuiSpriteFrame* pFrame = pAnim->GetFrame(ToBelow(mCurrentFrame));
   nuiRect src(pFrame->GetRect());
