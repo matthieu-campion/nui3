43d3459c4808935530be7975a44e0e3709d23af9
Sébastien Métrot <meeloo@libnui.net>
2010-12-18 21:59:35 +0100

    fixed buggy optimization

--- src/Renderers/nuiTexture.cpp
+++ src/Renderers/nuiTexture.cpp
@@ -681,13 +681,13 @@ void nuiTexture::TextureToImageCoord(nuiAltSize& x, nuiAltSize& y) const
 
 void nuiTexture::ImageToTextureCoord(nuiSize& x, nuiSize& y) const
 {
-  if (GetWidth())
+  if (mRealWidth)
     x /= GetWidth();
   else  if (mpImage && mpImage->GetWidth())
     x /= mpImage->GetWidth();
 
 
-  if (GetHeight())
+  if (mRealHeight)
     y /= GetHeight();
   else if (mpImage && mpImage->GetHeight())
     y /= mpImage->GetHeight();
@@ -695,12 +695,12 @@ void nuiTexture::ImageToTextureCoord(nuiSize& x, nuiSize& y) const
 
 void nuiTexture::TextureToImageCoord(nuiSize& x, nuiSize& y) const
 {
-  if (GetWidth())
+  if (mRealWidth)
     x *= GetWidth();
   else if (mpImage && mpImage->GetWidth())
     x *= mpImage->GetWidth();
 
-  if (GetHeight())
+  if (mRealHeight)
     y *= GetHeight();
   else if (mpImage && mpImage->GetHeight())
     y *= mpImage->GetHeight();
