38c0d364ec5686d142f72c5946c6d3a0189988e3
Sebastien Metrot <meeloo@meeloo.net>
2011-05-03 15:18:37 +0200

    halfway thru windows build fix

--- deps/ucdata/ucdata.h
+++ deps/ucdata/ucdata.h
@@ -26,15 +26,17 @@
  * $Id: ucdata.h,v 1.6 2001/01/02 18:46:20 mleisher Exp $
  */
 
-#ifndef WIN32
-#include <stdint.h>
-#else
-typedef char  int8_t;
-typedef short int16_t;
-typedef int   int32_t;
-typedef unsigned char  uint8_t;
+#ifdef _MSC_VER
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef signed short int16_t;
 typedef unsigned short uint16_t;
-typedef unsigned int   uint32_t;
+typedef signed int int32_t;
+typedef unsigned int uint32_t;
+typedef signed long long int64_t;
+typedef unsigned long long uint64_t;
+#else
+#include <stdint.h>
 #endif
 
 #ifdef __cplusplus
