47452945d06021738ab06ae1854d4858d9ecf996
vince <vincent.bongiorno@intua.net>
2012-09-06 16:57:52 +0200

    getting keyboard back

--- src/Window/Cocoa/nglWindow_Cocoa.mm
+++ src/Window/Cocoa/nglWindow_Cocoa.mm
@@ -576,7 +576,6 @@ NSDragOperation GetNSDragOperation(nglDropEffect Effect)
 ////////// Keyboard:
 - (BOOL)keyDown:(NSEvent *)theEvent
 {
-  return false; ///< FIXME
   NSString *chars = [theEvent characters];
   NSString *rawchars = [theEvent charactersIgnoringModifiers];
   nglString c((CFStringRef)chars);
